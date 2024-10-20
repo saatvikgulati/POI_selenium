d811918492d856bc80f9fe9f1a1dbe50bd5fcb51
Saatvik Gulati <saatugulati@gmail.com>
2024-10-20 19:45:23 +0530

    made changes

--- scraper.py
+++ scraper.py
@@ -13,52 +13,39 @@ def get_home_links(url):
     driver = webdriver.ChromiumEdge()
     try:
         driver.get(url)
-
-        counter = 1 
-        scraper_list = []
-        while True:
-            try:
-                # Fetch all property links
-                properties = driver.find_elements(By.XPATH, xpathpattern_home.format(counter))
-                properties_link = [element.get_attribute('href') for element in properties if element.get_attribute('href')]
-                if properties_link:
-                    scraper_list.extend(properties_link)
-                    counter += 1
-                else:
-                    break
-            except NoSuchElementException:
-                break
-            finally:
-                if len(scraper_list) > 1:
-                    print(f"{len(scraper_list)} links found")
-                    pd.DataFrame({'links':scraper_list}).to_csv(f'all_property_links.csv',index=False)
+        # Fetch all property links
+        WebDriverWait(driver,15).until(EC.presence_of_all_elements_located((By.XPATH, '//a[contains(@data-custominfo, "For Buyers")]')))
+        properties = driver.find_elements(By.XPATH, '//a[contains(@data-custominfo, "For Buyers")]')
+        properties_link = [element.get_attribute('href') for element in properties if element.get_attribute('href')]
+        if len(properties_link) > 1:
+            print(f"{len(properties_link)} links found")
+            pd.DataFrame({'links':properties_link}).to_csv(f'data/all_property_links.csv',index=False)
 
     finally:
         driver.quit()
 
-def get_area_details(link):
+def get_area_details(url):
     all_property_names = []
     all_property_types = []
     driver = webdriver.ChromiumEdge()
-    driver.get(link)
-    property_names_cnt=2
-    while True:
-        try:
-            property_names = driver.find_elements(By.XPATH, xpathpattern_areawise.format(property_names_cnt))
+    try:
+        for link in url:
+            driver.get(link)
+            WebDriverWait(driver,15).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.tupleNew__locationName, .ellipsis')))
+            property_names = driver.find_elements(By.CSS_SELECTOR, 'div.tupleNew__locationName, .ellipsis')
             property_name = [element.get_attribute('innerText').strip().replace('\r\n','').replace('\n','').lower() for element in property_names if element.get_attribute('innerText').strip().replace('\r\n','').replace('\n','').lower()]
             all_property_names.extend(property_name)
-            property_names_cnt+=2
-            if not property_name:
-                break
-        except NoSuchElementException:
-            break
-    property_types = driver.find_elements(By.CSS_SELECTOR,'.tupleNew__propType h2')
-    property_type = [element.get_attribute('span').strip()+element.get_attribute('innerText').strip().replace('\r\n', '').replace('\n', '').lower() for
-        element in property_types]
-    all_property_types.extend(property_type)
-    time.sleep(15)
-    # WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.pageComponent a')))
-    print(all_property_names)
-    print(all_property_types)
-    pd.DataFrame({'names':all_property_names}).to_csv(f'all_property_names_{link.split("/")[-1]}.csv',index=False)
-    driver.quit()
+            WebDriverWait(driver,15).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'h2.PseudoTupleRevamp__subHeading, .ellipsis, .PseudoTupleRevamp__projectHeading')))
+            property_types = driver.find_elements(By.CSS_SELECTOR,'h2.PseudoTupleRevamp__subHeading, .ellipsis, .PseudoTupleRevamp__projectHeading')
+            for element in property_types:
+                if property_types:
+                    spans = element.find_elements(By.TAG_NAME, 'span')
+                    span_text = [element.get_attribute('innerText').strip().replace('\r\n', '').replace('\n', '').lower() for
+                                 element in spans if spans]
+                    all_property_types.extend(span_text)
+            # WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.pageComponent a')))
+        print(all_property_types)
+        print(all_property_names)
+        pd.DataFrame({'names':all_property_names}).to_csv(f'data/all_property_names_{link.split("/")[-1]}.csv',index=False)
+    finally:
+        driver.quit()
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from scraper import *
 # get_home_links(url_home)
 df = pd.read_csv('all_property_links.csv')
-get_area_details(df['links'][0])
+links = df['links'].tolist()
+get_area_details(links)
